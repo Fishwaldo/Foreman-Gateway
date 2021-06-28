cd75e65f70a12bbce9b8d5fad0d7158e06d040c0
Justin Hammond <justin@dynam.ac>
2021-06-28 16:18:23 +0800

    Add this file

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego/v2/server/web/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["Foreman-Gateway/controllers:ForemanController"] = append(beego.GlobalControllerRouter["Foreman-Gateway/controllers:ForemanController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: "/",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
